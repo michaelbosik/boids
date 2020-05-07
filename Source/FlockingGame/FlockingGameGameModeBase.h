d43c4ec9af0f76f6c975b6c4faf3f8d136196925
Michael Bosik <michaelbosik@gmail.com>
2020-05-07 12:21:31 -0400

    implemented boids algorithm

---
+++ Source/FlockingGame/FlockingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlockingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOCKINGGAME_API AFlockingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
